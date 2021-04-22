68406271961658bb195b16f6a05a482d53181f41
Rosenornen <70527031+Rosenornen@users.noreply.github.com>
2021-04-22 16:33:52 +0200

    ff

---
+++ shared/db.js
@@ -0,0 +1,68 @@
+const { Connection, Request, TYPES} = require('tedious');
+const config = require('./config.json');
+
+var connection = new Connection(config);
+
+function startDb(){
+    return new Promise((resolve, reject) => {
+        connection.on('connect', (err) => {
+            if (err){
+                console.log('Connection failed')
+                reject(err)
+                throw(err);
+            } else {
+                console.log('Connected')
+                resolve();
+            }
+        })
+        connection.connect();
+    })
+}
+module.exports.sqlConnection = connection
+module.exports.startDb = startDb
+
+
+function insert(payload){
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO [users].[users] (name, email, gender, country, birthdate) VALUES (@name, @email, @gender, @country, @birthdate)`
+        const request = new Request(sql, (err) => {
+            if (err){
+                reject(err)
+                console.log(err)
+            }
+        });
+        request.addParameter('name', TYPES.VarChar, payload.name)
+        request.addParameter('email', TYPES.VarChar, payload.email)
+        request.addParameter('gender', TYPES.VarChar, payload.gender)
+        request.addParameter('country', TYPES.VarChar, payload.country)
+        request.addParameter('birthdate', TYPES.Date, payload.birthdate)
+
+        request.on('requestCompleted', (row) => {
+            console.log('User inserted', row);
+            resolve('user inserted', row)
+        });
+        connection.execSql(request)
+
+    });
+}
+module.exports.insert = insert
+
+function select(name) {
+    return new Promise((resolve, reject) => {
+         const sql = 'SELECT * FROM [datingapplication].[Users] where name = @name'
+        const request = new Request(sql, (err, rowcount) => {
+            if (err){
+                reject(err)
+                console.log(err)
+            } else if (rowcount == 0){
+                reject({message: 'User does not exits'})
+            }
+        });
+        request.addParameter('name', TYPES.VarChar, name)
+        request.on('row', (coloms) => {
+            resolve(colums)
+        });
+        connection.execSql(request)
+    })
+}
+module.exports.select = select
