7353ecaff740dffc4d62a0aad117c0f415cc8489
William <WilliamOlesen0210@hotmail.com>
2021-04-23 14:52:19 +0200

    regist function 1

---
+++ Client/signup.js
@@ -0,0 +1,18 @@
+// Henter HTML data
+let nameRegister = document.getElementById('');
+let passwordRegister = document.getElementById('');
+let emailRegister = document.getElementById('');
+
+// REGISTER Function
+function register(){
+    let registerData = {
+        name : nameRegister.value,
+        password : passwordRegister.value,
+        email: emailRegister
+    }
+// POST REQUEST til localhost
+
+
+
+}
+
