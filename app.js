26c6b1fc6d10a8efc60b55bfbeb198e70f23334c
SorenHoltorp <s-h-g@live.dk>
2021-04-23 14:34:14 +0200

    Changes

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require ('express');
 const app = express();
 const port = 9000; 
 
+/* - Kommer til at være noget vi skal bruge
+const signupRoute = require ("./controller/signupAPI")
+const loginRoute = require ("./controller/loginAPI")
+*/
 
 app.get("/",(req,res)=>{
     res.send("<h1>Gitlaid homepage</h1>")
 });
+
+/* -Kommer til at være noget vi skal bruge
+app.use("/signup", signupRoute);
+app.use("/", loginRoute )
+*/
 
 app.listen(port, () => {
     console.log(`Server-applikation lytter på http://localhost:${port}`)
