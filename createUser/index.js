e3c185573b2b89d346ff37cb5593f3cdb1d7f536
Rosenornen <70527031+Rosenornen@users.noreply.github.com>
2021-04-24 13:06:51 +0200

    Lidt mange ændringer

--- createUser/index.js
+++ createUser/index.js
@@ -1,4 +1,4 @@
-const db = require('../SERVER/shared/db');
+const db = require('../shared/db');
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.')
