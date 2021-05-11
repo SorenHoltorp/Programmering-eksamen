fc53f16184b751c7005f070ed3d98a105ec3cd63
William <WilliamOlesen0210@hotmail.com>
2021-05-11 09:56:50 +0200

    AAA test pattern

--- test.js
+++ test.js
@@ -4,16 +4,22 @@ chai.use(chaiHttp);
 
 let expect = require("chai").expect;
 
-  
+  // First Test
   it('it should log in', function() {   // <= No done callback
+    // 1. Arrange
+    let user = {
+      email:"emil@emil.com",
+      password:"1234"
+
+    }
+    // 2. Act
     chai.request('http://localhost:7071')
     .post('/api/login')
     .type("form")
-    .send({
-        email:"emil@emil.com",
-        password:"1234", 
-    })
+    .send(user)
     .end(function(err, res) {
+
+      // 3. Assert
       expect(res).to.have.status(123);    // <= Test completes before this runs
     });
   });
